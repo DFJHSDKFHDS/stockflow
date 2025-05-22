a544f97a41106bf0b01bb858ee76480628eaa1d1
ADS <adsfinalyear2025@gmail.com>
2025-05-22 19:12:59 +0000

    where i have to add image and what is the name of image and size also

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,10 +35,10 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="StockFlow" />
-        <link rel="apple-touch-icon" href="https://placehold.co/180x180.png?text=SFApple" />
-        <link rel="apple-touch-icon" sizes="152x152" href="https://placehold.co/152x152.png?text=SFApple" />
-        <link rel="apple-touch-icon" sizes="180x180" href="https://placehold.co/180x180.png?text=SFApple" />
-        <link rel="apple-touch-icon" sizes="167x167" href="https://placehold.co/167x167.png?text=SFApple" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/apple-touch-icon-167x167.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
